8da1ab47ba3d55d84c8b7762e60c9b286d70c477
Kineforce <lucas-sm2010@hotmail.com>
2021-02-23 01:02:32 -0300

    Double submit post

--- unstableChat/js/scripts_pwd.js
+++ unstableChat/js/scripts_pwd.js
@@ -4,7 +4,12 @@ $('#pass_form').submit(function(e) {
 
     pass_form = document.getElementById('pass_form');
 
+    pass_form_button = document.getElementById('submit_pwd');
+    $(pass_form_button).attr("disabled", true);
+
     var formData = new FormData(pass_form);
+
+    formData.append('pwd', '1');
 
     if (pass_form == ""){
 
@@ -24,13 +29,17 @@ $('#pass_form').submit(function(e) {
                     document.getElementById('feedback_message').innerHTML = 'Redirecting to unstableChat!';
                     setTimeout(function(){ 
                         window.location.href = './chat.php';
-                    }, 3000);
-                }else if (data.status = 'wrongpassword') {
+                    }, 2000);
+                }else if (data.status == 'wrongpassword') {
                     document.getElementById('feedback_message').innerHTML = 'Wrong password!';
                     setTimeout(function(){ 
                         window.location.href = './';
                     }, 2000);
-                } else {
+                } else if (data.status == 'nopassword') {
+                    document.getElementById('feedback_message').innerHTML = 'Insert a password!';
+                }else if (data.status == 'waitforit'){
+                    document.getElementById('feedback_message').innerHTML = 'You already send a request!';
+                }else {
                     document.getElementById('feedback_message').innerHTML = 'Contact support!';
                 }
             }
--- unstableChat/login.php
+++ unstableChat/login.php
@@ -14,87 +14,101 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 $username = isset($_POST['userName'])? $_POST['userName']: '';
 $color    = isset($_POST['color'])? $_POST['color']: '';
 $password = isset($_POST['userPass'])? $_POST['userPass']: '';
+$_SESSION['color_real'] = ($color != "")? $color : "";
 
-if ($color != ""){
+if (isset($_POST['pwd'])){
 
-    $_SESSION['color_real'] = $color;
+    if ($password != ""){
 
-}
+        include_once("conn.php");
+    
+        $username = $_SESSION['username'];
+        $userexists = isset($_SESSION['itexists'])? $_SESSION['itexists']: '';    
+    
+        if($userexists == 'false'){
+    
+            if ($_POST['token'] != $_SESSION['token']){
+                
+                $response_array['status'] = 'waitforit';
 
-if ($username == ""){
-
-    $response_array['status'] = 'invalidUser';
-
-}
-
-if (isset($username) && $username != ""){
-
-    $_SESSION['username'] = $username;
-
-    include_once("conn.php");
-
-    $query = "SELECT * FROM users WHERE USERNAME = ?;";
-
-    $result = sqlsrv_query($conn, $query , array($username));
-
-    $row = sqlsrv_fetch_array($result);
-
-    if (!$row){
-       
-        $_SESSION['itexists'] = 'false';
-        $_SESSION['color'] = $color;
-
-    } else {
-
-        $_SESSION['itexists'] = 'true';
-
-    }
-
-    $response_array['status'] = 'gotopwd';
-
-}
-
-if ($password != ""){
-
-    include_once("conn.php");
-
-    $username = $_SESSION['username'];
-    $userexists = isset($_SESSION['itexists'])? $_SESSION['itexists']: '';    
-
-    if($userexists == 'false'){
-
-        $insert_query = "INSERT INTO USERS (userName, userPwd, userColor)
-                         VALUES (?, ?, ?)";
-
-        $password = password_hash($password, PASSWORD_DEFAULT);                 
-
-        $result = sqlsrv_query($conn, $insert_query, array($username, $password, $_SESSION['color']));
-
-        $response_array['status'] = 'success';
-
-    }else {
-
-        $hash_query = " SELECT userPwd
-                        FROM   USERS
-                        WHERE  USERNAME = ?;";
-
-
-        $result_query = sqlsrv_query($conn, $hash_query, array($username) );
-
-        $hash_db_pass = sqlsrv_fetch_array($result_query);
-
-        $password = password_verify($password, $hash_db_pass[0]);
-
-        if ($password == true){
-
-            $response_array['status'] = 'success';
+            } else {
+    
+                $insert_query = "INSERT INTO USERS (userName, userPwd, userColor)
+                VALUES (?, ?, ?)";
+    
+                $password = password_hash($password, PASSWORD_DEFAULT);                 
+    
+                $result = sqlsrv_query($conn, $insert_query, array($username, $password, $_SESSION['color']));
+    
+                $response_array['status'] = 'success';
+    
+                $_SESSION['token'] = md5(session_id() . time());
+                
+    
+            }
+    
+        }else {
+    
+            $hash_query = " SELECT userPwd
+                            FROM   USERS
+                            WHERE  USERNAME = ?;";
+    
+    
+            $result_query = sqlsrv_query($conn, $hash_query, array($username) );
+    
+            $hash_db_pass = sqlsrv_fetch_array($result_query);
+    
+            $password = password_verify($password, $hash_db_pass[0]);
+    
+            if ($password == true){
+    
+                $response_array['status'] = 'success';
+            
+            } else {
         
+                $response_array['status'] = 'wrongpassword';
+            
+            }
+        }  
+    
+    } else {
+    
+        $response_array['status'] = "nopassword";
+    
+    }
+    
+} else {
+
+    if (isset($username) && $username != ""){
+
+        $_SESSION['username'] = $username;
+    
+        include_once("conn.php");
+    
+        $query = "SELECT * FROM users WHERE USERNAME = ?;";
+    
+        $result = sqlsrv_query($conn, $query , array($username));
+    
+        $row = sqlsrv_fetch_array($result);
+    
+        if (!$row){
+           
+            $_SESSION['itexists'] = 'false';
+            $_SESSION['color'] = $color;
+    
         } else {
     
-            $response_array['status'] = 'wrongpassword';
-        
+            $_SESSION['itexists'] = 'true';
+    
         }
-    }  
+    
+        $response_array['status'] = 'gotopwd';
+    
+    }else {
+    
+        $response_array['status'] = 'invalidUser';
+        
+    }
 }
 
 header('Content-type: application/json');
