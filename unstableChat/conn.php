d6cf1ef605f31402b97dc524dd52307ad9cf663a
Kineforce <lucas-sm2010@hotmail.com>
2021-02-25 18:30:09 -0300

    Criando banco de dados com as tabelas necessárias

--- unstableChat/conn.php
+++ unstableChat/conn.php
@@ -5,22 +5,36 @@ if (isset($_SESSION['username'])){
         
     header('Content-type: application/json');
 
-    /*$serverName = "PROMITERE\\SQLEXPRESS"; 
-    $connectionInfo = array( "Database"=>"messages");
-
-    $conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-    if( !$conn ) {
-        echo "Connection could not be established.<br />";
-    }*/
-
     $db = new SQLite3('messages.db');
 
     if ( !$db ){
         $response_array['status'] = "You got a error!";
         echo json_encode($response_array);
         die();
-    } 
+
+    } else {
+
+        $create_stored_messages = $db->prepare( "   CREATE TABLE IF NOT EXISTS STORED_MESSAGES (
+                                                    messageId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                                    userName TEXT NOT NULL,
+                                                    messageStamp TEXT NOT NULL,
+                                                    messageText TEXT NOT NULL
+                                                
+                                                )");
+
+        $create_stored_messages->execute();    
+
+        $create_users_table      = $db->prepare( "  CREATE TABLE IF NOT EXISTS USERS (
+                                                    userId INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                    userPwd TEXT NOT NULL,
+                                                    userColor TEXT NOT NULL,
+                                                    userName TEXT NOT NULL
+                                                
+                                                )");
+
+        $create_users_table->execute(); 
+
+    }
         
 } else {
 
