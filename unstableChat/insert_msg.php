84d91380bd68e03711c7e4d8c7c720e0fbf58a02
Kineforce <lucas-sm2010@hotmail.com>
2021-02-25 16:42:04 -0300

    Removendo código desnecessário

--- unstableChat/insert_msg.php
+++ unstableChat/insert_msg.php
@@ -20,36 +20,10 @@ if (isset($_SESSION['isValidated'])){
         $stmt_insert_message->bindValue(2, $messageText);
 
         $result_insert_message = $stmt_insert_message->execute();
+
+        $response_array['status'] = 'success';
+        echo json_encode($response_array['status']);
         
-        //$result = sqlsrv_query($conn, $query, array($username, $messageText));
-
-        $query_2 = $db->prepare("SELECT * FROM UPDATE_DIV");
-
-        //$result_2 = sqlsrv_query($conn, $query_2);
-
-        //$data = sqlsrv_fetch_array($result_2);
-
-        $result_2 = $query_2->execute();
-        $data = $result_2->fetchArray();
-
-        if ($data['databaseSession'] == 1){
-
-            $stmt_update_query = $db->prepare("UPDATE UPDATE_DIV SET databaseSession = 0");
-            $stmt_update_query->execute();
-            
-        }
-
-        if ($data['databaseSession'] == 0){
-
-            $stmt_update_query = $db->prepare("UPDATE UPDATE_DIV SET databaseSession = 1");
-            $stmt_update_query->execute();
-
-        }
-
-        $response_array['cookie_value'] = $data['databaseSession'];
-
-        echo json_encode($response_array);
-
     }
 
 }else {
