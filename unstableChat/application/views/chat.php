b95423f7340cebe084efe0fc81ab8ab4f898832f
Kineforce <lucas-sm2010@hotmail.com>
2021-03-18 00:16:04 -0300

    Ajustes no css e layout em geral

--- unstableChat/application/views/chat.php
+++ unstableChat/application/views/chat.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="shortcut icon" href="<?php echo BASE_URL(); ?>static/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo BASE_URL(); ?>static/css/style_messages.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
@@ -14,14 +14,14 @@
     
     <div class="container">
         <div class="box">
+            <div id="go_to_bottom">   
+            </div> 
             <div class="online_users">
                 <span>USERS</span>
                 <div class="user"> 
                 </div>
             </div> 
             <div class="chat">     
-                <div id="go_to_bottom">   
-                </div> 
                 <div class="message_box">
                 </div>   
                 <div class="insert_box">
